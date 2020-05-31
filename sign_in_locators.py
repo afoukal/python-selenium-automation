ccfd4ee756529b79ef7f5b36a6aaedfb9440abd4
afoukal <ana.foukal@gmail.com>
2020-05-31 16:19:29 -0500

    Locators

---
+++ sign_in_locators.py
@@ -0,0 +1,60 @@
+# Locators for Amazon Sign In page
+
+# Amazon logo:
+
+#By.XPATH : "//i[@class='a-icon a-icon-logo']"
+#By.XPATH : "//i[@aria-label='Amazon']"
+#By.XPATH : "//*[@role='img']"
+
+# Email field :
+
+#By.ID : "ap_email"
+#By.NAME : "email"
+#By.XPATH : "//*[@type='email']"
+
+# Continue button:
+
+#By.ID : "continue"
+#By.XPATH : "//*[@class='a-button-input']"
+#By.XPATH : "//*[@type='submit']"
+
+# Conditions of use:
+
+#By.XPATH : "//a[text()='Conditions of Use']"
+#By.XPATH : "//a[contains(@href, 'condition of use')]"
+
+# Privacy Notice:
+
+#By.XPATH : "//a[text()='Privacy Notice']"
+#By.XPATH : "//a[contains(@href, 'privacy_notice')]"
+
+# Need Help?
+
+#By.XPATH : "//span[@class='a-expander-prompt']"
+#By.LINK_TEXT : 'Need help?'
+
+# Forgot your password?
+
+#By.ID : "auth-fpp-link-bottom"
+#By.LINK_TEXT : 'Forgot your password?'
+
+# Other issues:
+
+#By.ID : "ap-other-signin-issues-link"
+#By.LINK_TEXT : "Other issues with Sign-In"
+
+# Create Amazon account:
+
+#By.ID : "createAccountSubmit"
+#By.LINK_TEXT : "Create your Amazon account"
+#By.XPATH : "//span[@class='a-button-inner']/a"
+
+
+
+
+
+
+
+
+
+
